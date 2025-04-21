238bc80bc2aa08743856e255fb5eaa39814138c0
eliooooooo <eliott.burkle@gmail.com>
2025-04-21 13:17:15 +0200

    Feat: use dataconnect sueries

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,21 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../components/welcome/welcome";
+import { getLocation } from '@firebasegen/default-connector';
+
+async function fetchLocations() {
+  try {
+    const result = await getLocation();
+    const locations = result.data?.location;
+
+    if (locations) {
+      console.log('Locations:', locations);
+    }
+  } catch (error) {
+    console.error('Error fetching locations:', error);
+  }
+}
+
+fetchLocations();
 
 export function meta({}: Route.MetaArgs) {
   return [
